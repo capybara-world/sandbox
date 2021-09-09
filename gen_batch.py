61b5c1f8103f6579b80e8982df55fda3cd4e2570
Dowland Aiello <dowlandaiello@gmail.com>
2021-09-09 14:30:23 -0400

    refactor gen_batch to have all main ops in a single def
    Signed-off-by: Dowland Aiello <dowlandaiello@gmail.com>

--- gen_batch.py
+++ gen_batch.py
@@ -17,204 +17,175 @@ def geo_mesh_center(o):
     return global_bbox_center
 
 
-# This file cannot be used as a module
-if __name__ != "__main__":
-    sys.exit(1)
+def main():
+    seed()
 
-seed()
+    # Determine where items should be placed on the capybaras
+    accessory_relative_locations = {}
 
-# Determine where items should be placed on the capybaras
-accessory_relative_locations = {}
+    # TODO: Remove light
+    parts = {
+        "Base Capybara",
+        "Base Capybara Eye(left)",
+        "Base Capybara Eye(right)",
+        "Base Capybara Nose",
+    #    "Light",
+    }
 
-# TODO: Remove light
-parts = {
-    "Base Capybara",
-    "Base Capybara Eye(left)",
-    "Base Capybara Eye(right)",
-    "Base Capybara Nose",
-#    "Light",
-}
+    capybara = bpy.data.objects["Base Capybara"]
+    eye = bpy.data.objects["Base Capybara Eye(left)"]
 
-capybara = bpy.data.objects["Base Capybara"]
-eye = bpy.data.objects["Base Capybara Eye(left)"]
+    capybaras = list(filter(
+        lambda obj: obj.name == "Base Capybara" or "capybara" in obj.name.lower() and "eye" not in obj.name.lower() and "nose" not in obj.name.lower(), bpy.data.objects
+    ))
 
-capybaras = list(filter(
-    lambda obj: obj.name == "Base Capybara" or "capybara" in obj.name.lower() and "eye" not in obj.name.lower() and "nose" not in obj.name.lower(), bpy.data.objects
-))
+    for obj in bpy.data.objects:
+        obj.select_set(False)
+        obj.hide_set(obj.name not in parts)
+        obj.hide_render = obj.name not in parts
 
-for obj in bpy.data.objects:
-    obj.select_set(False)
-    obj.hide_set(obj.name not in parts)
-    obj.hide_render = obj.name not in parts
+    # BEGIN BASE COAT COLOR RANDOMIZATION
 
-# BEGIN BASE COAT COLOR RANDOMIZATION
+    # Calculate the most common material to find the fur material
+    material_occurrences = {}
+    most_common, most_occ = None, 0
 
-# Calculate the most common material to find the fur material
-material_occurrences = {}
-most_common, most_occ = None, 0
+    for f in capybara.data.polygons:
+        mat_idx = f.material_index
 
-for f in capybara.data.polygons:
-    mat_idx = f.material_index
+        if mat_idx not in material_occurrences:
+            material_occurrences[mat_idx] = 0
 
-    if mat_idx not in material_occurrences:
-        material_occurrences[mat_idx] = 0
+        material_occurrences[mat_idx] += 1
 
-    material_occurrences[mat_idx] += 1
+        if (curr_occ := material_occurrences[mat_idx]) > most_occ:
+            most_occ = curr_occ
+            most_common = mat_idx
 
-    if (curr_occ := material_occurrences[mat_idx]) > most_occ:
-        most_occ = curr_occ
-        most_common = mat_idx
-
-base_coat_colors = (
-    capybara.material_slots[most_common]
-    .material.node_tree.nodes["ColorRamp"]
-    .color_ramp
-)
-
-# Determine where to place all accessories on the capybaras
-# The different types of accessories, each type containing items that cannot be placed on the same capybara
-# at the same time because they would overlap
-accessory_relative_locs = {}
-accessory_sets = []
-
-all_accessories = list(
-    filter(
-        lambda obj: hasattr(obj.data, "polygons")
-        and obj.name not in parts
-        and obj.name != "Cylinder"
-        and "capybara" not in obj.name.lower()
-        and (
-            len(obj.data.polygons) != len(eye.data.polygons)
-            or len(obj.data.vertices) != len(eye.data.vertices)
-            or len(obj.data.edges) != len(eye.data.edges)
-        ),
-        bpy.data.objects,
+    base_coat_colors = (
+        capybara.material_slots[most_common]
+        .material.node_tree.nodes["ColorRamp"]
+        .color_ramp
     )
-)
 
-for obj in all_accessories:
-    obj.hide_set(False)
+    # Determine where to place all accessories on the capybaras
+    # The different types of accessories, each type containing items that cannot be placed on the same capybara
+    # at the same time because they would overlap
+    accessory_relative_locs = {}
+    accessory_sets = []
 
-# The BVH trees of all capybaras, which with an item must intersect in order to be its child
-capy_trees = {}
-capy_geo_pos = {}
+    all_accessories = list(
+        filter(
+            lambda obj: hasattr(obj.data, "polygons")
+            and obj.name not in parts
+            and obj.name != "Cylinder"
+            and "capybara" not in obj.name.lower()
+            and (
+                len(obj.data.polygons) != len(eye.data.polygons)
+                or len(obj.data.vertices) != len(eye.data.vertices)
+                or len(obj.data.edges) != len(eye.data.edges)
+            ),
+            bpy.data.objects,
+        )
+    )
 
-for accessory in all_accessories:
-    # Determine which capybara the accessory is on
-    most_likely_base = (None, (0, 0.00))
+    for obj in all_accessories:
+        obj.hide_set(False)
 
-    # Find the distance between center of capy geometry and accessory geometry
-    bmh = bmesh.new()
-    bmh.from_mesh(accessory.data)
-    bmh.transform(accessory.matrix_world)
+    # The BVH trees of all capybaras, which with an item must intersect in order to be its child
+    capy_geo_pos = {}
 
-    bvh_tree_pre_norm = BVHTree.FromBMesh(bmh)
+    for accessory in all_accessories:
+        # Determine which capybara the accessory is on
+        most_likely_base = (None, 0.00)
 
-    # Find the distance between the center of capy geometry and accessory geometry
-    acc_geo_pos = geo_mesh_center(accessory)
+        # Find the distance between the center of capy geometry and accessory geometry
+        acc_geo_pos = geo_mesh_center(accessory)
 
-    for base in capybaras:
-        if base not in capy_trees:
-            capy_bmh = bmesh.new()
-            capy_bmh.from_mesh(base.data)
-            capy_bmh.transform(base.matrix_world)
+        for base in capybaras:
+            if base not in capy_geo_pos:
+                capy_geo_pos[base] = geo_mesh_center(base)
 
-            capy_trees[base] = BVHTree.FromBMesh(capy_bmh)
-            capy_geo_pos[base] = geo_mesh_center(base)
+            dist = abs(capy_geo_pos[base][1] - acc_geo_pos[1])
 
-        likelihood = len((bvh_tree_pre_norm.overlap(capy_trees[base])))
-        dist = abs((capy_geo_pos[base] - acc_geo_pos).length)
+            if dist <= most_likely_base[1] or (most_likely_base[0] is None and dist < 10.0):
+                most_likely_base = (base, dist)
 
-        print(likelihood, dist)
+        # Extraneous objects not part of the capybara accessories
+        if most_likely_base[0] is None:
+            continue
 
-        if likelihood > most_likely_base[1][0] and likelihood > 0 and (dist <= most_likely_base[1][1] or most_likely_base[0] is None):
-            most_likely_base = (base, (likelihood, dist))
+        base = most_likely_base[0]
 
-    # Extraneous objects not part of the capybara accessories
-    if most_likely_base[0] is None:
-        continue
+        print(accessory.name, base.name)
 
-    base = most_likely_base[0]
+        relative_loc = acc_geo_pos[1] - capy_geo_pos[base][1]
+        accessory_relative_locs[accessory.name] = relative_loc
 
-    relative_loc = accessory.location[1] - base.location[1]
-    accessory_relative_locs[accessory.name] = relative_loc
+        # Re-position the accessory
+        accessory.location.y = capybara.location[1] + relative_loc
+        bpy.context.view_layer.update()
 
-    # Re-position the accessory
-    accessory.location.y = capybara.location[1] + relative_loc
-    bpy.context.view_layer.update()
+        set_x_disps = []
 
-    bmh = bmesh.new()
-    bmh.from_mesh(accessory.data)
-    bmh.transform(accessory.matrix_world)
+        acc_geo_pos = geo_mesh_center(accessory)
 
-    bvh_tree = BVHTree.FromBMesh(bmh)
-    set_x_disps = []
+        # See if this accessory would overlap with an accessory in the set. If so, insert it in that set
+        for i, (pos, acc_set) in enumerate(accessory_sets):
+            displacement = abs(acc_geo_pos[0] - pos[0])
+            set_x_disps.append((i, displacement))
 
-    acc_geo_pos = geo_mesh_center(accessory)
+        try:
+            closest_set = min(set_x_disps, key=lambda item : item[1])
+            assert closest_set[1] <= 1.00
 
-    # See if this accessory would overlap with an accessory in the set. If so, insert it in that set
-    for i, (pos, acc_set) in enumerate(accessory_sets):
-        displacement = abs(acc_geo_pos[0] - pos[0])
-        set_x_disps.append((i, displacement))
+            accessory_sets[closest_set[0]][1].append(accessory.name)
+        except (ValueError, AssertionError):
+            accessory_sets.append((acc_geo_pos, [accessory.name]))
 
-    try:
-        closest_set = min(set_x_disps, key=lambda item : item[1])
-        assert closest_set[1] <= 1.00
+    for (i, (acc_set, set_members)) in enumerate(accessory_sets):
+        for mem in set_members:
+            bpy.data.objects[mem].hide_set(False)
+            bpy.data.objects[mem].hide_render = False
 
-        accessory_sets[closest_set[0]][1].append(accessory.name)
-    except (ValueError, AssertionError) as e:
-        accessory_sets.append((acc_geo_pos, [accessory.name]))
+        bpy.context.scene.render.filepath = f"/home/dowlandaiello/Downloads/hmm{i}.png"
+        bpy.ops.render.render(animation=False, use_viewport=False, write_still=True)
 
-for obj in bpy.data.objects:
-    obj.hide_set(True)
+        for mem in set_members:
+            bpy.data.objects[mem].hide_set(True)
+            bpy.data.objects[mem].hide_render = True
 
-acc_set, set_members = accessory_sets[0]
+    raise Exception()
 
-for accessory in set_members:
-    bpy.data.objects[accessory].hide_set(False)
+    # Generate 16 different colors per capy
+    for i in range(16):
+        # Uniform distribution of colors, but VERY rare translucent capybaras
+        hsv = [
+            np.random.beta(a=400, b=400) - 0.48,
+            np.random.beta(a=65, b=30),
+            np.random.beta(a=7, b=40),
+        ]
+        a = choices([1.0, 0.6], [0.95, 0.05])[0]
 
-raise Exception()
+        base_coat_colors.elements[1].color = (*colorsys.hsv_to_rgb(*hsv), a)
 
-for (i, (acc_set, set_members)) in enumerate(accessory_sets):
-    for mem in set_members:
-        bpy.data.objects[mem].hide_set(False)
-        bpy.data.objects[mem].hide_render = False
+        print(list(hsv))
 
-    bpy.context.scene.render.filepath = f"/home/dowlandaiello/Downloads/hmm{i}.png"
-    bpy.ops.render.render(animation=False, use_viewport=False, write_still=True)
+        # All animals have a difuse
 
-    for mem in set_members:
-        bpy.data.objects[mem].hide_set(True)
-        bpy.data.objects[mem].hide_render = True
+        hsv[1] -= 0.05
+        hsv[2] -= 0.075
 
-raise Exception()
+        base_coat_colors.elements[0].color = (*colorsys.hsv_to_rgb(*hsv), a)
 
-# Generate 16 different colors per capy
-for i in range(16):
-    # Uniform distribution of colors, but VERY rare translucent capybaras
-    hsv = [
-        np.random.beta(a=400, b=400) - 0.48,
-        np.random.beta(a=65, b=30),
-        np.random.beta(a=7, b=40),
-    ]
-    a = choices([1.0, 0.6], [0.95, 0.05])[0]
+        print(list(hsv))
 
-    base_coat_colors.elements[1].color = (*colorsys.hsv_to_rgb(*hsv), a)
+        # Export the capybara to a GLB
+        capybara.select_set(True)
+        # bpy.ops.export_scene.gltf(filepath=f"out/{capybara.name}_{''.join([str(i) for i in hsv])}.glb", use_selection=True, export_materials="EXPORT")
+        # bpy.ops.wm.save_as_mainfile(filepath="/home/dowlandaiello/hi.blend")
+        # capybara.select_set(False)
 
-    print(list(hsv))
-
-    # All animals have a difuse
-
-    hsv[1] -= 0.05
-    hsv[2] -= 0.075
-
-    base_coat_colors.elements[0].color = (*colorsys.hsv_to_rgb(*hsv), a)
-
-    print(list(hsv))
-
-    # Export the capybara to a GLB
-    capybara.select_set(True)
-    # bpy.ops.export_scene.gltf(filepath=f"out/{capybara.name}_{''.join([str(i) for i in hsv])}.glb", use_selection=True, export_materials="EXPORT")
-    # bpy.ops.wm.save_as_mainfile(filepath="/home/dowlandaiello/hi.blend")
-    # capybara.select_set(False)
-
+# This file cannot be used as a module
+if __name__ == "__main__":
+    main()
